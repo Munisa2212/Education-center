7e9f18a346b399310b69ca070372dbc293a366ea
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-03-24 17:52:47 +0500

    changed

--- validation/registration.validation.js
+++ validation/registration.validation.js
@@ -3,7 +3,7 @@ const joi = require("joi")
 const RegistrationValidation = joi.object({
     learningCenter_id: joi.number().required().min(0),
     branch_id: joi.number().required().min(0),
-    user_id: joi.number().required().min(0),
+    user_id: joi.number().min(0),
     date: joi.date().required()
 })
 
