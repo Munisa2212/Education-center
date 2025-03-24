bceafa0ea56a3a419457b4aced9349fa32acc020
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-03-24 17:42:53 +0500

    like module and validation done

---
+++ models/like.module.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize")
+const {db} = require("../config/db")
+
+const Like = db.define(
+    "Likes",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER
+        },
+        learningCenter_id: {
+            type: DataTypes.INTEGER
+        }
+    }
+)
+
+module.exports = Like
