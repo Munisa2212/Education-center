dec6dc15249dba673a43ab64f493d048094b8ab8
Nurali11 <boltaboyevnurali55@gmal.com>
2025-03-25 16:08:36 +0500

    branch field and subject

--- routes/branch.route.js
+++ routes/branch.route.js
@@ -1,4 +1,4 @@
-const { Branch , Center} = require('../models/index.module')
+const { Branch , Center, Region, Field, Subject} = require('../models/index.module')
 const Branch_valadation = require('../validation/branch.valadation')
 const express = require('express')
 const route = express()
@@ -15,23 +15,29 @@ route.get('/', async (req, res) => {
 
 route.post('/', async (req, res) => {
   try {
+    let region= await Region.findByPk(region_id)
     const { error } = Branch_valadation.validate(req.body)
-    if (error)
-<<<<<<< HEAD
-      return res.status(400).send({ message: error.details[0].message })
+    if (error) return res.status(400).send({ message: error.details[0].message })
 
     let {learningCenter_id, region_id, field_id, subject_id} = req.body
 
     let center= await Center.findByPk(learningCenter_id)
     if (!center) return res.status(404).send({ message: 'Center not found' })
     
-    let region= await Region.findByPk(region_id)
     if (!region) return res.status(404).send({ message: 'Region not found' })
+    
+    for (let i = 0; i < subject_id.length; i++) {
+      let subject= await Subject.findByPk(subject_id[i])
+      if (!subject) return res.status(404).send({ message: `Subject with ${e} id not found` })
+    }
+  
+    for (let i = 0; i < field_id.length; i++) {
+      let field= await Field.findByPk(field_id[i])
+      if (!field) return res.status(404).send({ message: `Field with ${e} id not found` })
+      }
 
+    center.update({branch_number: center.branch_number + 1})
 
-=======
-      return res.status(400).send({ message: error.message })
->>>>>>> fc3953c2eac08e5beae2ab61d5b58919e94f3776
     let NewBranch = await Branch.create(req.body)
 
     res.status(201).send({ NewBranch })
