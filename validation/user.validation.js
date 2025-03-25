77e077adba33c43d086c95b106851dfcea6acfe3
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-03-25 14:04:30 +0500

    Save local changes before pulling

--- validation/user.validation.js
+++ validation/user.validation.js
@@ -1,17 +1,17 @@
 const joi = require("joi")
 
-const UserValidation = {
+const UserValidation = joi.object({
     name: joi.string().required(),
     email: joi.string().email().required(),
     password: joi.string().required(),
     phone: joi.string().length(13).required(),
     image: joi.string().required(),
     role: joi.string().valid("USER", "ADMIN", "SUPER-ADMIN", "CEO").required()
-}
+})
 
-const LoginValidation = {
+const LoginValidation = joi.object({
     email: joi.string().email().required(),
     password: joi.string().required()
-}
+})
 
 module.exports = UserValidation
